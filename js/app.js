de2214ae41f68ca2b33e703d180683d8995da14b
Glenn Gijsberts <glenngijsberts95@gmail.com>
2017-09-19 22:47:48 +0200

    Added logic of retrieving data to app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,62 @@
 //Vue code here
+
+const CRYPTOCOMPARE_API_URI = "https://www.cryptocompare.com";
+const COINMARKETCAP_API_URI = "https://api.coinmarketcap.com";
+
+const UPDATE_INTERVAL = 60 * 1000;
+
+setInterval(function() {
+	app.getCoins();
+}, UPDATE_INTERVAL);
+
 var app = new Vue({
 	el: "#app",
 
 	data: {
-		message: 'test'
+		
+		coins: [],
+		coinData: {}
+
+	},
+
+	methods: {
+
+		getCoinData: function(){
+
+			var app = this;
+
+			axios.get('https://www.cryptocompare.com/api/data/coinlist/')
+				.then(function(res) {
+					app.coinData = res.data.Data;
+					app.getCoins();
+				}).catch(function(err) {
+					app.getCoins();
+					console.error(err);
+				});
+
+		},
+
+		getCoins: function() {
+
+			var app = this;
+
+			axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=10')
+				.then(function(res) {
+					app.coins = res.data;
+				}).catch(function(err) {
+					console.error(err);	
+				});
+
+		},
+
+		getCoinImage: function(symbol) {
+			return CRYPTOCOMPARE_API_URI + this.coinData[symbol].ImageUrl;
+		}
+
+	}, //end of methods
+
+	created: function() {
+		this.getCoinData();
 	}
 
-});
+}); //end of vue instance
